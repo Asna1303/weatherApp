14f42d6f31611a6b08b02e6b1353d01159ca90e2
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-24 14:26:55 +0530

    data is declared

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -21,6 +21,7 @@ if(element[0].value===""){
 }
 let url="https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}London&appid=${api_key}";
 let response=await fetch(url);
+let data = await response.json();
     }
   return (
     <div className='container'>
